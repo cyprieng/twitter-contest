69efa50ca33d5b125390a8606572eb9d68916498
Cyprien G <cyprien.guillemot@gmail.com>
2015-08-15 23:14:54 +0200

    First release

---
+++ run.py
@@ -0,0 +1,4 @@
+from twitter_contest import TwitterContest
+
+t = TwitterContest()
+t.run()
---
+++ twitter_contest.py
@@ -0,0 +1,64 @@
+import twitter
+import re
+
+
+class TwitterContest():
+
+    def __init__(self):
+        """
+        Init the twitter API.
+        """
+        consumer_key = 'LJjCS3u6Bi8BaiEGr7fTZnJgy'
+        consumer_secret = 'eCedGJimotK6VmODo8BYxIU8Ma1WdEwKHKVkEC9coWDFrse3Ef'
+        access_token = '3424055837-bfJzBwxRgFSqneW6gIRTbb7WIN07GAcriAWCAVa'
+        access_token_secret = 'IeX9LBAxjCrP9h3MPRauMnTw5PxkGl5vMnT5E5h1dXKHr'
+
+        self.api = twitter.Api(consumer_key=consumer_key,
+                      consumer_secret=consumer_secret,
+                      access_token_key=access_token,
+                      access_token_secret=access_token_secret)
+
+    def get_contest_tweets(self):
+        """
+        Get the tweets correponding to a contest
+        """
+        search_word = ['gagner', 'RT']
+        tweets = []
+        test = self.api.GetSearch(' '.join(search_word), count=100)
+
+        # Remove RTs
+        for tweet in test:
+            p = re.compile(ur'^RT @')
+            if not re.match(p, tweet.text):
+                tweets.append(tweet)
+
+        return tweets
+
+    def participate_in_contest(self, tweet):
+        """
+        Participate in the contest of the given tweet.
+
+        Args:
+            tweet: tweet correponding to the contest.
+        """
+        # Follow the user if necessary
+        if 'follow' in tweet.text or 'Follow' in tweet.text:
+            try:
+                self.api.CreateFriendship(tweet.user.id)
+            except:
+                pass
+
+        # Retweet
+        try:
+            self.api.PostRetweet(tweet.id)
+        except:
+            pass
+
+    def run(self):
+        """
+        Run detection of contest and participate.
+        """
+        tweets = self.get_contest_tweets()
+
+        for tweet in tweets:
+            self.participate_in_contest(tweet)
